970e7fd78f65711fa8c57c45b19533daf60ff9e3
Michael Sawula <msawula@users.noreply.github.com>
2016-11-01 00:37:15 -0500

    finished

---
+++ db/migrate/20161101045810_create_instructors.rb
@@ -0,0 +1,10 @@
+class CreateInstructors < ActiveRecord::Migration
+  def change
+    create_table :instructors do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101041635) do
+ActiveRecord::Schema.define(version: 20161101045810) do
 
   create_table "guesses", force: :cascade do |t|
     t.integer  "first_num"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20161101041635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "outcome"
+  end
+
+  create_table "instructors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
